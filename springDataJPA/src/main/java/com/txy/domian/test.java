959e61c37bea699f7b0641451a6737ab9810ec07
taoxiangyu <taoxiangyujava@163.com>
2024-09-03 16:31:47 +0800

    测试

---
+++ springDataJPA/src/main/java/com/txy/domian/test.java
@@ -0,0 +1,12 @@
+package com.txy.domian;
+
+/**
+ * @Author : taoxy
+ * @Date: 2024/9/3 16:31
+ * @Description :
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
